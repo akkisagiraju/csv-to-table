8c18f3a1468ce2f1f0b78622d1b4cbebb586ccbf
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-10-12 22:40:44 +0530

    turn this into an IIFE

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-const { readCSV } = require('./csvReader');
+(function () {
+  const { readCSV } = require('./csvReader');
 
-// select the app element
-const app = document.querySelector('#app');
+  // select the app element
+  const app = document.querySelector('#app');
 
-// create an file input element
-const fileUploader = document.createElement('input');
-fileUploader.setAttribute('type', 'file');
+  // create a file input element and append it to the app
+  const fileUploader = document.createElement('input');
+  fileUploader.setAttribute('type', 'file');
+  app.appendChild(fileUploader);
 
-// append the file input to the app element
-app.appendChild(fileUploader);
-
-// listen to the file input changes and run a function to read the file uploaded
-fileUploader.addEventListener('change', readCSV);
+  // listen to the file input changes and run a function to read the file uploaded
+  fileUploader.addEventListener('change', readCSV);
+})();
